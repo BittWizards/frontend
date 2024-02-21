dfc5c835d6669fd940741d91180c7c6384a21ee5
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2024-02-21 13:22:38 +0300

    add router

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux"
 import App from "./App"
 import { store } from "./app/store"
 import "./index.css"
+import { BrowserRouter } from "react-router-dom"
 
 const container = document.getElementById("root")
 
@@ -12,13 +13,16 @@ if (container) {
 
   root.render(
     <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
     </React.StrictMode>,
   )
 } else {
   throw new Error(
-    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
+    `Root element with ID 'root' was not found in the document.
+    Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.`,
   )
 }
