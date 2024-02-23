72e2341416f44a23dbb76af063a450a9f7c77a7e
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2024-02-23 13:25:39 +0300

    feat: added ambassador card list

---
+++ src/widgets/AmbassadorCard/types/types.ts
@@ -0,0 +1,13 @@
+type TAmbassadorCardProps = {
+  data: {
+    id: string;
+    avatar: string;
+    name: string;
+    surname: string;
+    secondname: string;
+    position: string;
+    date: string;
+  };
+};
+
+export type { TAmbassadorCardProps };
