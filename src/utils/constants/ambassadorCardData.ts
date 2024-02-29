da273396d4d34f0ddf8e2b2c7064eaa811f2763a
SofiaFrikina <sofi.frikina@yandex.ru>
2024-02-29 16:16:28 +0300

    поиск

--- src/utils/constants/ambassadorCardData.ts
+++ src/utils/constants/ambassadorCardData.ts
@@ -1,45 +1,5 @@
 import avatar from 'src/shared/icons/userAvatar.png';
 
-type TCardData = {
-  id: string;
-  avatar: string;
-  telegram: string;
-  name: string;
-  surname: string;
-  secondname: string;
-  position: string;
-  date: string;
-  statusActive: boolean;
-  promocode: string;
-  activationDate: string;
-  trackingNumber: string;
-  userStatus: string;
-  phone: string;
-  email: string;
-  city: string;
-  merch: {
-    id: string;
-    merchType: string;
-    merchSize: string;
-    date: string;
-    quantity: string;
-    price: string;
-  }[];
-  reviews: number;
-  promocodes: {
-    id: string;
-    promocode: string;
-    date: string;
-  }[];
-  content: {
-    id: string;
-    date: string;
-    link: string;
-    platform: string;
-    fileCounter: string;
-  }[];
-};
-
 const userCardsData = [
   {
     id: '1',
