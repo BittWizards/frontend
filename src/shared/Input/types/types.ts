0f8c0827789160b45884739c7155bfe5ecc97480
Roman Tyulyukin <tylykin2013@yandex.ru>
2024-02-29 15:25:49 +0300

    clear errors

--- src/shared/Input/types/types.ts
+++ src/shared/Input/types/types.ts
@@ -1,18 +1,10 @@
 import { Noop, RefCallBack } from 'react-hook-form';
 
 interface IInput {
-  // value?: string | number;
-  // placeholder?: string;
-  // type: string;
-  // disabled?: boolean;
-  field: {
-    onChange: (...event: any[]) => void;
-    onBlur: Noop;
-    value: any;
-    disabled?: boolean | undefined;
-    name: 'name';
-    ref: RefCallBack;
-  };
+  value?: string | number;
+  placeholder?: string;
+  type: string;
+  disabled?: boolean;
 }
 
 export type { IInput };
--- src/shared/Input/ui/Input.tsx
+++ src/shared/Input/ui/Input.tsx
@@ -1,47 +1,20 @@
 import { FC } from 'react';
 import { IInput } from '../types/types';
+
+// import { Input } from '@mui/material';
 import { TextField } from '@mui/material';
 
 import style from './Input.module.scss';
 
-const Innput: FC<IInput> = ({field}) => {
+const Innput: FC<IInput> = ({ value, placeholder, type, disabled }) => {
   return (
-    <TextField
-    {...field}
-      id="outlined-basic"
-      variant="outlined"
-      sx={{
-        borderRadius: '4px',
-        border: '1px solid #939393',
-        '& .MuiOutlinedInput-root': {
-          '&.Mui-focused fieldset': {
-            borderColor: 'white',
-          },
-          '& input:hover + fieldset': {
-            borderColor: '#512da8',
-          },
-          '& input:invalid + fieldset': {
-            borderColor: '#cd4e2e',
-          },
-          '& input:disabled + fieldset': {
-            backgroundColor: 'rgba(71, 70, 70, 0.6)',
-          },
-          '& input': {
-            color: '#ebeef4',
-            padding: '12px 16px',
-            fontFamily: 'YSText',
-            lineHeight: 1.2,
-          },
-        },
-      }}
+    <input
+      type={type}
+      className={style.input}
+      value={value}
+      placeholder={placeholder}
+      disabled={disabled}
     />
-    // <input
-    //   type={type}
-    //   className={style.input}
-    //   value={value}
-    //   placeholder={placeholder}
-    //   disabled={disabled}
-    // />
   );
 };
 
