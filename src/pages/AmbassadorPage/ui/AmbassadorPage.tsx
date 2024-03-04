619fc0411107bb0d50f0bff317cc184cdd14b6e0
Natasha <84683371+NatashaSolntseva@users.noreply.github.com>
2024-03-05 00:27:28 +0300

    Update AmbassadorPage.tsx
    added import import  ChangeEvent, MouseEvent

--- src/pages/AmbassadorPage/ui/AmbassadorPage.tsx
+++ src/pages/AmbassadorPage/ui/AmbassadorPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { ChangeEvent, MouseEvent, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import type { User } from 'src/utils/constants/types/types';
 
