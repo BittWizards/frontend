44646123ca6ebd06b3b76d5824edc2e7c9886a38
Roman <tylykin2013@yandex.ru>
2024-03-07 03:06:29 +0300

    fix

--- src/shared/FormContainer/ui/FormContainer.tsx
+++ src/shared/FormContainer/ui/FormContainer.tsx
@@ -23,11 +23,8 @@ const FormContainer: FC<IFormContainer> = ({
   const submitForm = (data: Object) => {
     onSubmit(data);
   };
-    onSubmit(data);
-  };
 
   const methods = useForm({
-    defaultValues: defaultValues || {},
     defaultValues: defaultValues || {},
   });
 
