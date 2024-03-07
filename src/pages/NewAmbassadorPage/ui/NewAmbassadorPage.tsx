cf1f08186b6831e77cb3e6a2b8c45e246d703d20
Roman <tylykin2013@yandex.ru>
2024-03-07 03:11:55 +0300

    add state

--- src/pages/NewAmbassadorPage/ui/NewAmbassadorPage.tsx
+++ src/pages/NewAmbassadorPage/ui/NewAmbassadorPage.tsx
@@ -1,4 +1,3 @@
-
 import { Navbar } from 'src/widgets/NavBar';
 import { QuestionnaireProfileInfo } from 'src/entities/QuestionnaireProfileInfo';
 import { QuestionnaireForm } from 'src/entities/QuestionnaireForm';
@@ -6,11 +5,21 @@ import { QuestionnaireForm } from 'src/entities/QuestionnaireForm';
 import { navbarLinks } from 'src/utils/constants/navLinks';
 import { FormContainer } from 'src/shared/FormContainer';
 import style from './NewAmbassadorPage.module.scss';
+import { useAppDispatch, useAppSelector } from 'src/app/store/hooks';
+import { selectQuestionnaire, setIsEdit } from 'src/app/store/reducers/questionnaire/model/questionnaireSlice';
+import { useEffect } from 'react';
 
 const NewAmbassadorPage = () => {
   const submitForm = (data: Object) => {
     console.log(data);
   };
+
+  const { isEdit } = useAppSelector(selectQuestionnaire);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(setIsEdit(true))
+  }, [])
 
   return (
     <div className={style.main}>
@@ -22,8 +31,8 @@ const NewAmbassadorPage = () => {
           submitButtonLabel="Сохранить"
           cancelButtonLabel="Отменить"
         >
-          <QuestionnaireProfileInfo isEdit />
-          <QuestionnaireForm isEdit />
+          <QuestionnaireProfileInfo />
+          <QuestionnaireForm />
         </FormContainer>
       </div>
     </div>
