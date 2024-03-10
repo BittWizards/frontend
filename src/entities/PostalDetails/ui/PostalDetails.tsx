76287746eb0327177a19bfa468c3ce1537454ce5
Roman <tylykin2013@yandex.ru>
2024-03-10 21:12:33 +0300

    add func

--- src/entities/PostalDetails/ui/PostalDetails.tsx
+++ src/entities/PostalDetails/ui/PostalDetails.tsx
@@ -8,8 +8,8 @@ const PostalDetails: FC<IPostalDetails> = () => (
   <FieldsetContainer title="Почтовые реквизиты">
     <Input type="text" placeholder="Страна" name="country" />
     <Input type="text" placeholder="Город" name="city" />
-    <Input type="text" placeholder="Адрес" name="adress" />
-    <Input type="text" placeholder="Индекс" name="index" />
+    <Input type="text" placeholder="Адрес" name="street_home" />
+    <Input type="text" placeholder="Индекс" name="post_index" />
   </FieldsetContainer>
 );
 
