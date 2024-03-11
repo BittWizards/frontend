aa96cf0f797b3dd7b7cd8e6d3abf990faa14bdc6
Roman <tylykin2013@yandex.ru>
2024-03-11 03:43:31 +0300

    fix button

--- src/utils/constants/api.ts
+++ src/utils/constants/api.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = process.env.REACT_APP_BASE_URL || '';
-// export const BASE_URL = 'https://ambassadors.sytes.net';
+// export const BASE_URL = process.env.REACT_APP_BASE_URL || '';
+export const BASE_URL = 'https://ambassadors.sytes.net';
