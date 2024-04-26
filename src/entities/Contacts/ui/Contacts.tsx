b00382a5e0f6c3746c5f851a3ec0464014c77132
Aleksey <129634706+aleksey2299-1@users.noreply.github.com>
2024-04-27 02:52:16 +0300

    Fixed fieldset inputs

--- src/entities/Contacts/ui/Contacts.tsx
+++ src/entities/Contacts/ui/Contacts.tsx
@@ -5,7 +5,6 @@ import { useAppSelector } from 'src/app/store/hooks';
 import { selectQuestionnaire } from 'src/app/store/reducers/questionnaire/model/questionnaireSlice';
 import type { IContacts } from '../types/types';
 
-
 import telegram from '../../../shared/icons/telegramIcon.svg';
 import email from '../../../shared/icons/mail.svg';
 import phone from '../../../shared/icons/phone.svg';
@@ -13,23 +12,22 @@ import phone from '../../../shared/icons/phone.svg';
 import style from './Contacts.module.scss';
 
 const Contacts: FC<IContacts> = () => {
-
-  const {isEdit} = useAppSelector(selectQuestionnaire)
+  const { isEdit } = useAppSelector(selectQuestionnaire);
 
   return (
     <FieldsetContainer title="Контактная информация">
       <div className={`${style.inputIcons} ${isEdit && style.gap}`}>
         <img src={telegram} className={style.icon} alt="telegram" />
-        <Input type="string" placeholder="@name" name="tg_acc" />
+        <Input type="string" placeholder="username" name="tg_acc" />
       </div>
 
       <div className={`${style.inputIcons} ${isEdit && style.gap}`}>
         <img src={email} className={style.icon} alt="email" />
-        <Input type="email" placeholder="@mail.ru" name="email" />
+        <Input type="email" placeholder="example@mail.ru" name="email" />
       </div>
       <div className={`${style.inputIcons} ${isEdit && style.gap}`}>
         <img src={phone} className={style.icon} alt="phone" />
-        <Input type="phone" placeholder="+7 " name="phone" />
+        <Input type="phone" placeholder="+7(999)999-99-99" name="phone" />
       </div>
     </FieldsetContainer>
   );
--- src/shared/FieldsetContainer/ui/FieldsetContainer.tsx
+++ src/shared/FieldsetContainer/ui/FieldsetContainer.tsx
@@ -4,21 +4,10 @@ import type { IFieldsetContainer } from '../types/types';
 import style from './FieldsetContainer.module.scss';
 
 const FieldsetContainer: FC<IFieldsetContainer> = ({ children, title }) => (
-  <label className={style.label}>
+  <div className={style.label}>
     {title}
     <fieldset className={style.fieldset}>{children}</fieldset>
-  </label>
+  </div>
 );
 
 export default FieldsetContainer;
-
-// const FieldsetContainer: FC<IFieldsetContainer> = ({ children, title, controlId }) => (
-//   <label className={style.label} htmlFor={controlId}>
-//     {title}
-//     <fieldset className={style.fieldset}>{children}</fieldset>
-//   </label>
-// );
-
-// export default FieldsetContainer;
-
-// TODO убрать ошибку линтера htmlFor={controlId}
